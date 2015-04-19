60489f6c7253b37ef7b8c95b06d02bee251ee7aa
Felix <Felix@ULTRA>
2015-04-19 19:11:51 +0200

    Added Javadoc

--- src/util/parser/ParserInput.java
+++ src/util/parser/ParserInput.java
@@ -1,10 +1,42 @@
 package util.parser;
 
+/**
+ * {@link ParserInput} represents the input for a Parser and allows to read one
+ * or more characters from the start of the input and potentially discard them.
+ * 
+ * @author Felix
+ * @version 1.1
+ *
+ */
 public interface ParserInput {
-	public void confirm();
-	public void reset();
+	/**
+	 * Reads the first char without discarding it. This does not advance the
+	 * cursor. Consecutive calls to this {@link ParserInput#peek()} should
+	 * return the same char.
+	 * 
+	 * @return the next char from the input.
+	 */
 	public char peek();
-	public String peek(int length);
-	public char read();
-	public String read(int length);
+	
+	/**
+	 * Reads the first char without discarding it. This does not advance the
+	 * cursor. Consecutive calls to this {@link ParserInput#peek()} should
+	 * return the same char.
+	 * 
+	 * @param n number of chars to read.
+	 * @return the next n chars from the input in a String, might be shorter if the end of the input is reached.
+	 */
+	public String peek(int n);
+	
+	/**
+	 * Advances the inputs cursor by 1 characters.
+	 */
+	public void advance();
+	
+	/**
+	 * Advances the inputs cursor by n characters.
+	 * 
+	 * @param n number of characters to advance by.
+	 */
+	public void advance(int n);
 }
--- src/util/parser/ProductionParser.java
+++ src/util/parser/ProductionParser.java
@@ -1,5 +1,22 @@
 package util.parser;
 
+/**
+ * A {@link ProductionParser} represents a process of a Parser, to try parse a
+ * part of an input, depending on the result of the parsing of the last part.
+ * 
+ * @author Felix
+ * @version 1.0
+ *
+ */
 public interface ProductionParser {
+	/**
+	 * Tries parsing the next part of input.
+	 * 
+	 * @param o
+	 *            The result of the parsing of the last part.
+	 * @param input
+	 *            The input trimmed of all parts that have already been parsed.
+	 * @return the result of the parsing, null if the attempt to parse failed.
+	 */
 	public Object tryParse(Object o, ParserInput input);
 }
